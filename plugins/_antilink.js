2dad55334be18ac8bf6801bafd354cf13a097360
oktetosupport <botzaky11@gmail.com>
2022-12-17 16:22:51 +0700

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,22 +1,20 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.db.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
-
-  if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
-    let thisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-    if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-      if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "Admin mah bebas ygy :'v" : `\n\nlink group terdeteksi dan ${global.namabot} bukan admin jadi tidak bisa ngekick!`}`)
-    if (isBotAdmin) {
-      m.reply(` *「 ANTILINK 」* \n\nLink Group Terdeteksi, bye Kamu Akan Di Kick!!`.trim())
-      await this.delay(500)
-      await this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-    }
-  }
-  return true
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
+	if (m.isBaileys && m.fromMe) return !0
+	if (!m.isGroup) return !1
+	let chat = global.db.data.chats[m.chat]
+	const isGroupLink = linkRegex.exec(m.text)
+	if (chat.antiLink && isGroupLink && !isAdmin) {
+		if (isBotAdmin) {
+			const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+			if (m.text.includes(linkThisGroup)) return !0
+		}
+		if (!m.fromMe && isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
+		await conn.sendMessage(m.chat, { text: `@${(m.sender || '').replace(/@s\.whatsapp\.net/g, '')} *terdeteksi* mengirim Link Group!`, mentions: [m.sender] }, { quoted: fkontak })
+	}
+	return true
 }
 
 module.exports = handler
